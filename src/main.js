cff119d3686b35c2cc4d1d126767a92ccad172fd
Jason M <jbird206@gmail.com>
2020-02-05 11:01:53 -0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,13 +1,6 @@
-import { triangle } from './triangle';
+import $ from 'jquery';
+import { Triangle } from './../src/triangle.js';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-
-$(document).ready(function() {
-  $('#ping-pong-form').submit(function(event) {
-    event.preventDefault();
-    var goal = $('#goal').val();
-    var output = pingPong(goal);
-    output.forEach(function(element) {
-      $('#solution').append("<li>" + element + "</li>");
-    });
-  });
-});
+import { Triangle } from './triangle.js';
